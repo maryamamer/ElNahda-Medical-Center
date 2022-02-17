59e76b44264f47ae8bcb46ef6a0a6d1bf0a9ece1
Nada494 <67652725+Nada494@users.noreply.github.com>
2022-02-17 14:42:58 +0200

    adding profile and login (#3)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import AboutUs from "./components/AboutUs"
 import Doctors from "./components/Doctors"
 import patient_portal from "./components/patient_portal"
 import HomePage from "./components/Home"
-
 import Footer from "./components/Footer"
+import ProfilePage from "./components/ProfilePage";
+import Login from "./components/Login";
+import Forget from "./components/Forget";
 
 
 function App() {
@@ -24,6 +26,10 @@ function App() {
             <Route path={"/AboutUs"} exact component={AboutUs} />
             <Route path={"/Doctors"} exact component={Doctors} />
             <Route path={"/patientPortal"} exact component={patient_portal} />
+
+            <Route path={"/ProfilePage"} exact component={ProfilePage} />
+            <Route path={"/Login"} exact component={Login} />
+          <Route path={"/Forget"} exact component={Forget} />
           </Switch>
           <Footer />
         </Router>
