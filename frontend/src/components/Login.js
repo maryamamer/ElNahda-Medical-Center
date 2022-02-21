0f5bb8c1d894f0302b2462ea5c2ad85fa1238d12
mera <merna7asan@gmail.com>
2022-02-21 22:01:15 +0200

    updating about us page+

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -9,47 +9,47 @@ function Login() {
 
         <>
 
-            <section class="h-100" id="login">
-                <div class="container h-100 opacity-0.4;">
-                    <div class="row justify-content-md-center h-100 ">
-                        <div class="card-wrapper">
-                            <div class="brand">
-                            <h4 class="card-title text-dark">تسجيل الدخول</h4>
+            <section className="h-100" id="login">
+                <div className="container h-100 opacity-0.4;">
+                    <div className="row justify-content-md-center h-100 ">
+                        <div className="card-wrapper">
+                            <div className="brand">
+                            <h4 className="card-title text-dark">تسجيل الدخول</h4>
                             </div>
-                            <div class="card-body">
+                            <div className="card-body">
                                 
-                                <form method="post" class="my-login-validation" novalidate onSubmit={loginuser}>
-                                    <div class="form-group">
-                                        <label for="email">اسم المسنخدم</label>
-                                        <input id="email" type="email" class="form-control" name="email" required />
-                                        <div class="invalid-feedback">
+                                <form method="post" className="my-login-validation" novalidate onSubmit={loginuser}>
+                                    <div className="form-group ">
+                                        <label for="email" className=" text-light">اسم المسنخدم</label>
+                                        <input id="email" type="email" className="form-control" name="email" required />
+                                        <div className="invalid-feedback">
                                             من فضلك ادخل بريد ألكتروني صحيح
                                         </div>
                                     </div>
 
-                                    <div class="form-group" />
-                                    <label for="password">كلمة السر
+                                    <div className="form-group " />
+                                    <label for="password" className=" text-light">كلمة السر
 
                                     </label>
-                                    <input id="password" type="password" class="form-control" name="password" required data-eye />
-                                    <div class="invalid-feedback">
+                                    <input id="password" type="password" className="form-control" name="password" required data-eye />
+                                    <div className="invalid-feedback">
                                         يجب ادخال كلمة السر
                                     </div>
 
 
-                                    <div class="form-group" />
-                                    <div class="custom-checkbox custom-control">
-                                        <input type="checkbox" name="remember" id="remember" class="custom-control-input" />
-                                        <label for="remember" class="custom-control-label">تذكرني
-                                            <a href="forgot.html" class="float-right">
+                                    <div className="form-group" />
+                                    <div className="custom-checkbox custom-control">
+                                        <input type="checkbox" name="remember" id="remember" className="custom-control-input" />
+                                        <label for="remember" className="custom-control-label text-light">تذكرني
+                                            <a href="forgot.html" className="float-right">
                                                 هل نسيت كلمة السر ؟
                                             </a>
                                         </label>
                                     </div>
 
 
-                                    <div class="form-group m-0" />
-                                    <input type="submit" class="btn btn-primary btn-block" value="تسجيل الدخول"  />
+                                    <div className="form-group m-0" />
+                                    <input type="submit" className="btn btn-primary btn-block" value="تسجيل الدخول"  />
 
 
                                 </form>
