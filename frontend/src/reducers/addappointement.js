de13432cd1baf6e7e2f5743ef87def2da6e185d8
maramkhaled <maramraslan45@gmail.com>
2022-02-22 00:46:28 +0200

    edit-appointment

--- frontend/src/reducers/addappointement.js
+++ frontend/src/reducers/addappointement.js
@@ -1,3 +1,11 @@
+import {
+    ADD_APPOINTMENT,
+    ADD_Patient_APPOINTMENT,
+    ADD_Doctor_APPOINTMENT,
+    REM_APPOINTMENT
+
+   
+} from '../actions/types';
 
 const INITIAL_STATE = {
     access: localStorage.getItem('access'),
@@ -8,13 +16,18 @@ const INITIAL_STATE = {
 export default function addapp(state = INITIAL_STATE, action) {
     const { type, payload } = action;
   switch (type) {
-    case "ADD_APP":
+    case ADD_APPOINTMENT:
+      return {
+          ...state,
+        app: payload
+    };
+    case ADD_Patient_APPOINTMENT:
       return {
           ...state,
         app: payload
     };
 
-    case "REM_APP":
+    case REM_APPOINTMENT:
       return [...state.filter((app) => app.id !== action.payload)];
 
     default:
